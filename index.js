90cc06466fefbd4e9b9b59a2cbd95e2dc47a5361
21kkfy <21kkfy@gmail.com>
2023-08-02 17:33:54 +0300

    Attempt to add MongoDB

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-// index.js
-
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
-const itemRoutes = require('./routes/items');
+const mongoose = require('mongoose'); // Import Mongoose
+const cors = require('cors'); // Import CORS middleware
 
 const app = express();
+const port = 3000;
 
-
-// Enable CORS
+// Use CORS middleware to allow cross-origin requests
 app.use(cors());
+
+// Connect to your MongoDB database
+const dbURI = 'mongodb://localhost:27017/demo'; // Replace with your database connection URI
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 // Middleware to parse incoming JSON data
 app.use(bodyParser.json());
 
-// Mount the item routes
-app.use('/api', itemRoutes); // Use '/api' as a base path for itemRoutes
+// Define the item schema and model
+const itemSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+// Define the route to handle POST requests for adding items
+app.post('/api/items', (req, res) => {
+    const newItem = new Item(req.body);
+
+    newItem.save()
+        .then(item => {
+            res.json(item);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Failed to add the item.' });
+        });
+});
 
 // Start the server
-const port = 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
